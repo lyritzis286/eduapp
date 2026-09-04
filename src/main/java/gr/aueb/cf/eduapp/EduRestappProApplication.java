b73c00ba7d0d039fc73894ec2fb87c5c02162269
lyritzis286 <lyritzis286@gmail.com>
2026-09-04 14:32:09 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/eduapp/EduRestappProApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.eduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduRestappProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduRestappProApplication.class, args);
+    }
+
+}
