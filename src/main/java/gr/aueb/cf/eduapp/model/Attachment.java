a00eca1b3fa52c5752a1f21b7572b1b08f182361
lyritzis286 <lyritzis286@gmail.com>
2026-09-04 20:25:27 +0300

    add Attachment class for managing file attachments and link to PersonalInfo

---
+++ src/main/java/gr/aueb/cf/eduapp/model/Attachment.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.eduapp.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@Table(name = "attachments")
+public class Attachment extends AbstractEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(name = "saved_name", nullable = false, unique = true)
+    private String savedName;
+
+    @Column(name = "file_path", nullable = false, length = 1024)
+    private String filePath;
+
+    @Column(name = "content_type", nullable = false)
+    private String contentType;
+
+    @Column(nullable = false)
+    private String extension;
+}
--- src/main/java/gr/aueb/cf/eduapp/model/PersonalInfo.java
+++ src/main/java/gr/aueb/cf/eduapp/model/PersonalInfo.java
@@ -28,4 +28,17 @@ public class PersonalInfo extends AbstractEntity {
 
     @Column(name = "municipality_of_registration", nullable = false)
     private String municipalityOfRegistration;
+    @Setter(AccessLevel.PACKAGE)
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "amka_file_id")
+    private Attachment amkaFile;
+
+    public void addAmkaFile(Attachment amkaFile) {
+        this.amkaFile = amkaFile;
+
+    }
+
+    public void removeAmkaFile(Attachment amkaFile) {
+        this.amkaFile = null;
+    }
 }
