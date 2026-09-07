99e8a162a11d8e74aa2f77f5470bb2cb691ee42e
lyritzis286 <lyritzis286@gmail.com>
2026-09-07 18:03:33 +0300

    add TeacherRepository and UserRepository for managing Teacher and User entities

---
+++ src/main/java/gr/aueb/cf/eduapp/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.eduapp.repository;
+
+import gr.aueb.cf.eduapp.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    Optional<User> findByUuid(UUID uuid);
+    Optional<User>findByUuidAndDeletedFalse(UUID uuid);
+
+
+    @EntityGraph(attributePaths = {"role", "role_capabilities"})
+    Optional<User>findByUsername(String username);
+
+    @EntityGraph(attributePaths = {"role", "role_capabilities"})
+    Optional<User>findByUsernameAndDeletedFalse(String username);
+
+
+
+}
