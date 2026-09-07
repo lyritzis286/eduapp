ef58c24d5ba8280564c1eb1ce15e7854ef2debce
lyritzis286 <lyritzis286@gmail.com>
2026-09-07 17:28:02 +0300

    add methods to manage user association in Teacher class

--- src/main/java/gr/aueb/cf/eduapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/eduapp/model/Teacher.java
@@ -43,6 +43,16 @@ public class Teacher extends AbstractEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    public void addUser(User user) {
+        this.user = user;
+        user.setTeacher(this);
+    }
+
+    public void removeUser(User user) {
+        this.user = null;
+        user.setTeacher(null);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Teacher teacher)) return false;
