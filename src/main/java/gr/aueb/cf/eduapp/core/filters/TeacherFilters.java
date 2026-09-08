4aa7549bf7197e97e4dd7960f29fd1736f6b9320
lyritzis286 <lyritzis286@gmail.com>
2026-09-08 20:57:28 +0300

    add Mapper and TeacherFilters classes for mapping and filtering Teacher entities

---
+++ src/main/java/gr/aueb/cf/eduapp/core/filters/TeacherFilters.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.eduapp.core.filters;
+
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Setter
+@Builder
+
+public class TeacherFilters {
+
+    private UUID uuid;
+    private String vat;
+    private String amka;
+    private String lastname;
+    private String deleted;
+    private String region;
+
+}
---
+++ src/main/java/gr/aueb/cf/eduapp/mapper/Mapper.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.eduapp.mapper;
+
+import gr.aueb.cf.eduapp.dto.*;
+import gr.aueb.cf.eduapp.model.PersonalInfo;
+import gr.aueb.cf.eduapp.model.Teacher;
+import gr.aueb.cf.eduapp.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapper {
+
+    public Teacher mapToTeacherEntity(TeacherInsertDTO dto) {
+        Teacher teacher = new Teacher();
+        teacher.setFirstName(dto.firstname());
+        teacher.setLastName(dto.lastname());
+        teacher.setVat(dto.vat());
+
+        UserInsertDTO userInsertDTO = dto.userInsertDTO();
+
+        User user = new User();
+        user.setUsername(userInsertDTO.username());
+        user.setPassword(userInsertDTO.password());
+
+        teacher.addUser(user);
+
+        PersonalInfoInsertDTO personalInfoInsertDTO = dto.personalInfoInsertDTO();
+
+        PersonalInfo personalInfo = new PersonalInfo();
+        personalInfo.setAmka(personalInfoInsertDTO.amka());
+        personalInfo.setIdentityNumber(personalInfoInsertDTO.identityNumber());
+        personalInfo.setPlaceOfBirth(personalInfoInsertDTO.placeOfBirth());
+        personalInfo.setMunicipalityOfRegistration(personalInfoInsertDTO.municipalityOfRegistration());
+
+        teacher.setPersonalInfo(personalInfo);
+        return teacher;
+    }
+
+    public TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
+        return new TeacherReadOnlyDTO(
+                teacher.getUuid().toString() , teacher.getLastName(),
+                teacher.getFirstName(),
+                teacher.getVat(), teacher.getRegion().getName());
+
+    }
+}
