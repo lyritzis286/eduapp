5b507c85b819c394853ec4b26deffb082c18a7af
lyritzis286 <lyritzis286@gmail.com>
2026-09-14 21:14:52 +0300

    refactor SecurityConfiguration; increase BCryptPasswordEncoder strength to enhance password security

--- src/main/java/gr/aueb/cf/eduapp/security/SecurityConfiguration.java
+++ src/main/java/gr/aueb/cf/eduapp/security/SecurityConfiguration.java
@@ -103,6 +103,6 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 }
